00498a6c79a50b38123e05df0f00082143d74994
rahul yadav <ry219613@gmail.com>
2021-10-08 02:23:13 +0530

    print alphabet is vowel or consonant

---
+++ src/bridgelab/com/vowelconsonant.java
@@ -0,0 +1,27 @@
+package bridgelab.com;
+
+import java.util.Scanner;
+
+public class vowelconsonant {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		char alphabet;
+		System.out.print("Enter any character : ");
+		alphabet = scanner.next().charAt(0);
+		if(alphabet == 'a' || alphabet =='e' || alphabet =='i' || alphabet == 'o'|| alphabet == 'u' ) {
+			System.out.println("Alphabet is vowel");
+			
+		}
+		else {
+			System.out.println("Alphabet is Consonant");
+		}
+	}
+	
+}	
+	
+
+
+
+	
+
+
